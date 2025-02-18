14a2ca2acdc82652091f2a96ae147c400205f342
Tai Groot <tai@taigrr.com>
2025-02-18 00:34:16 -0800

    add placehomer unexported unittypes for later

--- structs.go
+++ structs.go
@@ -13,3 +13,18 @@ type Unit struct {
 	Sub         string
 	Description string
 }
+
+var unitTypes = []string{
+	"automount",
+	"device",
+	"mount",
+	"path",
+	"scope",
+	"service",
+	"slice",
+	"snapshot",
+	"socket",
+	"swap",
+	"target",
+	"timer",
+}
